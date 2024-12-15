202c90b268f005f143fa1c355f8216cbb64f727b
mohamed-achich <mohamed.achich27@gmail.com>
2024-12-15 16:51:38 +0100

    feat(orders): added service health check

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,20 @@ import { OrdersModule } from './orders/orders.module';
 import { databaseConfig } from './config/database.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SecurityModule } from './security/security.module';
+import { HealthModule } from './health/health.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
     TypeOrmModule.forRoot(databaseConfig),
-    OrdersModule
+    SecurityModule,
+    OrdersModule,
+    AuthModule,
+    HealthModule 
   ],
   controllers: [AppController],
   providers: [AppService],
